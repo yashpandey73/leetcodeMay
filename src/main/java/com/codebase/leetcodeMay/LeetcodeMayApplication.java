10de9933a05011dc1fb08e3a0ff489a149b96b48
PANDEY Yash EQ-WW <mspr1129@orange.com>
2020-05-01 22:47:20 +0530

    first commit

---
+++ src/main/java/com/codebase/leetcodeMay/LeetcodeMayApplication.java
@@ -0,0 +1,13 @@
+package com.codebase.leetcodeMay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeMayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeMayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebase/leetcodeMay/LeetcodeMayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebase.leetcodeMay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeMayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
